2acf2e1101fc13edb70e4ad3835e0b9a103aadbc
nabia-sheikh <nabiasheikh2222@gmail.com>
2021-07-20 15:09:23 +0500

    Update

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,11 +7,11 @@ const settings = {
 
 //SEO Related settings
 const seo = {
-  title: "Ashutosh's Portfolio",
+  title: "Nabia Sheikh",
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Nabia Sheikh Portfolio",
     type: "website",
     url: "http://ashutoshhathidara.com/",
   },
